5a41fa7b32380216c87cf36a4b60bbbbd30821fd
Carlitava <166379048+Carlitava@users.noreply.github.com>
2024-05-01 15:51:39 +0200

    Add files via upload

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,11 @@
+SELECT 
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity,  
+    ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*p.purchase_price,2) AS margin
+FROM {{ref("stg_raw__raw_gz_sales")}} s
+LEFT JOIN {{ref("stg_raw__raw_gz_product")}} p 
+    USING (products_id)
